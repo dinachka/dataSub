0773a4b118b0509b56ca02e9a93dd472aa2c653e
dina <dinachernykh1@gmail.com>
2022-02-24 14:05:32 +0300

    form

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,32 @@
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="outlined-basic" label="Card Number" variant="outlined" />
+        <TextField id="outlined-basic" label="Expiration Date" variant="outlined" />
+
+        <TextField id="outlined-basic" label="CVV" variant="outlined" />
+
+        <TextField id="outlined-basic" label="Amount" variant="outlined" />
+        <Button variant="contained" disabled>
+          Оплатить
+        </Button>
+      </Box>
+    </>
   );
 }
 
